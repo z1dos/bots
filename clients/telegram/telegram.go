1542a2fad9742539fc782f24dacc264a2c58a556
z1dos <georg.awdeew2017@yandex.ru>
2023-09-08 20:30:12 +0700

    first commit

---
+++ clients/telegram/telegram.go
@@ -0,0 +1,57 @@
+package telegram
+
+import (
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+
+	"testbottwo/client/e"
+)
+
+type Client struct {
+	host     string
+	basePath string
+	client   http.Client
+}
+
+func New(host string, token string) Client {
+	return Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
+
+func (c *Client) Updates(offset int, limit int) ([]Update, error) {
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("offset", strconv.Itoa(limit))
+
+}
+
+func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+	defer func ()  {err = e.Wrap(errMsg, err)}()
+	const errMsg = "can't do request"
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, e.Wrap(errMsg, err)
+	}
+
+	req.URL.RawFragment = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, e.Wrap(errMsg, err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/z1dos/bots
+
+go 1.19
